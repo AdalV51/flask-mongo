732017fdc3fe078491a38e8a3259c49f00fc0a54
AdalV51 <vazquezromanadalberto@gmail.com>
2023-08-08 16:05:54 -0600

    Formatted app.py using Black

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
+import random
 from flask import Flask
 import pymongo
-import random
 
 # Connect to MongoDB
 client = pymongo.MongoClient(
-    "mongodb://mongo-db:27017", 
+    "mongodb://mongo-db:27017",
     username="adal",
     password="password",
     authSource="admin",
-    authMechanism='SCRAM-SHA-256')
+    authMechanism="SCRAM-SHA-256",
+)
 
 db = client["miapp"]
 collection = db["customers"]
@@ -16,49 +17,72 @@ collection = db["customers"]
 
 # Create a function to get a random name
 def get_name():
-    names = ["John", "Mary", "Bob", "Jane", "Peter", "Paul", "Mark", "Luke", "Matthew", "James"]
+    names = [
+        "John",
+        "Mary",
+        "Bob",
+        "Jane",
+        "Peter",
+        "Paul",
+        "Mark",
+        "Luke",
+        "Matthew",
+        "James",
+    ]
     return random.choice(names)
+
 
 # Create a function to get a random address
 def get_address():
-    addresses = ["Highway 37", "Main Street", "First Avenue", "Second Street", "Park Avenue", "Elm Street", "Maple Avenue", "Oak Street", "Washington Avenue", "Lake Street"]
+    addresses = [
+        "Highway 37",
+        "Main Street",
+        "First Avenue",
+        "Second Street",
+        "Park Avenue",
+        "Elm Street",
+        "Maple Avenue",
+        "Oak Street",
+        "Washington Avenue",
+        "Lake Street",
+    ]
     return random.choice(addresses)
+
 
 # Create a function to get a dict with a random name and address
 def get_record():
-    record = {
-        "name": get_name(),
-        "address": get_address()
-    }
+    record = {"name": get_name(), "address": get_address()}
     return record
 
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
     return "Hello, Docker!"
 
-@app.route('/add')
+
+@app.route("/add")
 def add_record():
     record = get_record()
     collection.insert_one(record)
     return "Record added."
 
-@app.route('/list')
+
+@app.route("/list")
 def list_records():
     result = ""
-    for x in collection.find():
-        result += str(x) + "<br>"
+    for record in collection.find():
+        result += str(record) + "<br>"
     return result
 
-@app.route('/deleteall')
+
+@app.route("/deleteall")
 def delete_all():
     collection.delete_many({})
     return "All records deleted."
 
 
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0')
-
-
+    app.run(host="0.0.0.0")
